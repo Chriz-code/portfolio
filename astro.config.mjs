81214c37e5de258842958a18d802f4faa816f516
Chris <78105414+Chriz-code@users.noreply.github.com>
2025-11-04 20:44:15 +0100

    Updated base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://chriz-code.github.io',
-    base: '/portfolio'
+    site: 'https://chriz-code.github.io'
 });
